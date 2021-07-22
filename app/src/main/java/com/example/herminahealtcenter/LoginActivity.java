69cdcac69a66e9962901678803af9201b878446a
restuardananto <stuard.phone@gmail.com>
2021-07-22 16:24:52 +0800

    update loginactiviy

--- app/src/main/java/com/example/herminahealtcenter/LoginActivity.java
+++ app/src/main/java/com/example/herminahealtcenter/LoginActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActiviry extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_activiry);
+        setContentView(R.layout.activity_login_activity);
     }
 }
