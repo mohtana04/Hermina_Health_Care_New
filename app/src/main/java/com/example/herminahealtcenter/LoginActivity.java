88ed0e1b5f08609e67298e0ac1a3d7cc0d8adde7
restuardananto <stuard.phone@gmail.com>
2021-07-27 23:21:37 +0800

    update XML activity login 210727 (hide Notification Bar)

--- app/src/main/java/com/example/herminahealtcenter/LoginActivity.java
+++ app/src/main/java/com/example/herminahealtcenter/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.herminahealtcenter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -32,6 +33,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login_activity);
 
         editTextNomr = (EditText) findViewById(R.id.ETnomorcm);
