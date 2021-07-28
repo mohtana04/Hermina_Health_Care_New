036199bfa2ca90a87bd2afd685deae5d17f0ab08
restuardananto <stuard.phone@gmail.com>
2021-07-28 15:27:04 +0800

    update210728 (dashboard activity dan fragment transaction)

--- app/src/main/java/com/example/herminahealtcenter/DashboardActivity.java
+++ app/src/main/java/com/example/herminahealtcenter/DashboardActivity.java
@@ -1,14 +1,63 @@
 package com.example.herminahealtcenter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.herminahealtcenter.fragment.HomeFragment;
+import com.example.herminahealtcenter.fragment.InformationFragment;
+import com.example.herminahealtcenter.fragment.ProfileFragment;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private Button Btninfo, Btnbrd, Btnprof;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Btninfo = findViewById(R.id.btninformasi);
+        Btnbrd = findViewById(R.id.btnberanda);
+        Btnprof = findViewById(R.id.btnprofile);
+
+        Btnbrd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(
+                        R.id.framefragment, new HomeFragment()
+                );
+                ft.commit();
+            }
+        });
+
+        Btninfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(
+                        R.id.framefragment, new InformationFragment()
+                );
+                ft.commit();
+            }
+        });
+
+        Btnprof.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                ft.replace(
+                        R.id.framefragment, new ProfileFragment()
+                );
+                ft.commit();
+            }
+        });
+
+
+
     }
 }
