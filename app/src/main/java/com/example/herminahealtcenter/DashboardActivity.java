2cf38bd997d2246216c431d10529f9edfff16ed7
restuardananto <stuard.phone@gmail.com>
2021-08-03 11:19:57 +0800

    20210802 (pembuatan slide viewpager selesai)

--- app/src/main/java/com/example/herminahealtcenter/DashboardActivity.java
+++ app/src/main/java/com/example/herminahealtcenter/DashboardActivity.java
@@ -7,10 +7,13 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import com.example.herminahealtcenter.adapter.VpAdapterClass;
 import com.example.herminahealtcenter.fragment.HomeFragment;
 import com.example.herminahealtcenter.fragment.InformationFragment;
 import com.example.herminahealtcenter.fragment.ProfileFragment;
@@ -36,7 +39,46 @@ public class DashboardActivity extends AppCompatActivity {
         Btnbrd = findViewById(R.id.btnberanda);
         Btnprof = findViewById(R.id.btnprofile);
         viewPager = findViewById(R.id.VPberanda);
+
+        FragmentManager fm = this.getSupportFragmentManager();
+        viewPager.setAdapter((PagerAdapter) (new VpAdapterClass(fm)));
+        viewPager.setOffscreenPageLimit(3);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                DashboardActivity.this.chageTabs(position);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        viewPager.setCurrentItem(0);
+        Btninfo.setBackgroundResource(R.drawable.active_back);
     }
 
+    private final void chageTabs(int position) {
+        if (position == 0) {
+            Btninfo.setBackgroundResource(R.drawable.active_back);
+            Btnbrd.setBackgroundResource(R.drawable.inactive_back);
+            Btnprof.setBackgroundResource(R.drawable.inactive_back);
+        }
+        if (position == 1) {
+            Btninfo.setBackgroundResource(R.drawable.inactive_back);
+            Btnbrd.setBackgroundResource(R.drawable.active_back);
+            Btnprof.setBackgroundResource(R.drawable.inactive_back);
+        }
+        if (position == 2) {
+            Btninfo.setBackgroundResource(R.drawable.inactive_back);
+            Btnbrd.setBackgroundResource(R.drawable.inactive_back);
+            Btnprof.setBackgroundResource(R.drawable.active_back);
+        }
+    }
 
 }
