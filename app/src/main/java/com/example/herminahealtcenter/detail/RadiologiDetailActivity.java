31e4df4e5b86f714556b769f8be475eb1f6c403d
restuardananto <stuard.phone@gmail.com>
2021-08-24 12:15:05 +0800

    20210824 (update UI radiologi header dan detail)

---
+++ app/src/main/java/com/example/herminahealtcenter/detail/RadiologiDetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.herminahealtcenter.detail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.herminahealtcenter.R;
+
+public class RadiologiDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radiologi_detail);
+    }
+}
